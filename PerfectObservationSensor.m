bd4080276f5e638ca8c74c7addc5f39f2d7712d2
Edward Mortimer (student) <2131094M@student.gla.ac.uk>
2019-02-25 15:28:38 +0000

    Sensor Model now fully works, can detect closest point purely geometrically. It has been tested in the test environment, now it needs to return the closest point for both sections

--- PerfectObservationSensor.m
+++ PerfectObservationSensor.m
@@ -46,11 +46,7 @@ classdef PerfectObservationSensor < handle
                 r = obj.d_limit; % Sensor limit and circle radius 
                 
                 D = det([x1, x2; y1, y2]);
-                
-                                 
-                x_m = (x_int1 - x_int2)/2; 
-                y_m = (y_int1 - y_int2)/2;
-                
+
                 % Wall edges
                 x_limits = [x1, x2];
                 y_limits = [y1, y2];
@@ -61,7 +57,7 @@ classdef PerfectObservationSensor < handle
                 [xRight, I2] = max(x_limits);
                         
                 leftEdge = [x_limits(I1), y_limits(I1)];
-                rightEdge = [x_limit(I2), y_limits(I2)];
+                rightEdge = [x_limits(I2), y_limits(I2)];
               
                 
                 if (r*dr)^2 - D^2 > 0  % Discriminant, postive = intersections, negative = no intersections
@@ -74,6 +70,12 @@ classdef PerfectObservationSensor < handle
                     
                     x_array = [x_int1, x_int2];
                     y_array = [y_int1, y_int2];
+                    
+                                    
+                                 
+                    x_m = (x_int1 + x_int2)/2; 
+                    y_m = (y_int1 + y_int2)/2;
+                
                     
                     % Determine Upper Half and if it cuts sensor range
                     headingVector = [0, 1];
@@ -92,7 +94,7 @@ classdef PerfectObservationSensor < handle
                         c3 = 0;
                         
                         % Intersection of lines 1 and 2
-                        x12 = (c2 - c1)/(m1 - m2)
+                        x12 = (c2 - c1)/(m1 - m2);
                         y12 = m1*x12 + c1;
                         
                         % Intersection of line 1 and 3
@@ -100,28 +102,36 @@ classdef PerfectObservationSensor < handle
                         y13 = m1*x13 + c1;
        
                         
-%                         if norm([x1, y1]) < r && obj.VectorAngle(headingVector, [x1, y1]) < obj.alpha
-%                             LeftLimit 
-%                         end
-
-                       LeftLine = atan2(y12/x12) == obj.alpha && norm([x12, y12]) < r;
-                       RightLine = atan2(y13/x13) == obj.alpha && norm([x13, y13]) < r;
+                       %-----Calculating arc intersections as booleans-----
+                     
                        
+                       
+                       %Calculating if intersections occur  along arc line edge
+                       LeftLine = round(atan2(y12,x12), 4) == round(pi/2 + obj.alpha, 4) && norm([x12, y12]) < r;
+                       RightLine = round(atan2(y13,x13), 4) == round(pi/2 - obj.alpha, 4) && norm([x13, y13]) < r;
+                       
+                      
                        [x_min, Imin] = min(x_array);
                        [x_max, Imax] = max(x_array);
- 
+                       
+                       % Calculating if intersections occur on the arc curv
+        
+
+                       
                        arcSection_1 = all([r*cos(pi/2 + obj.alpha), r*sin(pi/2 + obj.alpha)] < [x_array(Imin), y_array(Imin)]); 
-                       arcSection_1 = arcSection_1 && all([x_array(Imin), y_array(Imin)] < [r*cos(pi/2 - obj.alpha), r*sin(pi/2 - obj.alpha)]);
+                       arcSection_1 = arcSection_1 && all([x_array(Imin), y_array(Imin)] < [r*cos(pi/2 - obj.alpha), r]);
+
                             
                        arcSection_2 = all([r*cos(pi/2 + obj.alpha), r*sin(pi/2 + obj.alpha)] < [x_array(Imax), y_array(Imax)]); 
-                       arcSection_2 = arcSection_2 && all([x_array(Imax), y_array(Imax)] < [r*cos(pi/2 - obj.alpha), r*sin(pi/2 - obj.alpha)]);
-                       
-                       
-                       if LineSection && arcSection_1
-                           arcLimits = [x12, y12l; x_array(Imin), y_array(Imin)];
+                       arcSection_2 = arcSection_2 && all([x_array(Imax), y_array(Imax)] < [r*cos(pi/2 - obj.alpha), r]);
 
-                       elseif LineSection && arcSection_2
-                           arcLimits = [x12, y12l; x_array(Imax), y_array(Imax)];
+                       
+                       
+                       if LeftLine && arcSection_1
+                           arcLimits = [x12, y12; x_array(Imin), y_array(Imin)];
+
+                       elseif LeftLine && arcSection_2
+                           arcLimits = [x12, y12; x_array(Imax), y_array(Imax)];
                            
                        elseif RightLine && arcSection_1 
                            arcLimits = [x13, y13; x_array(Imin), y_array(Imin)];
@@ -130,13 +140,16 @@ classdef PerfectObservationSensor < handle
                            arcLimits = [x13, y13; x_array(Imax), y_array(Imax)];
 
                        elseif LeftLine && RightLine
-                           arcLimits = [x12, y12; x13, y13]
+                           arcLimits = [x12, y12; x13, y13];
 
                        elseif arcSection_1 && arcSection_2
                            arcLimits = [x_array(Imin), y_array(Imin); x_array(Imax), y_array(Imax)];
     
                        else
                            output = [1, 1];
+                           line([x1, x2], [y1, y2]);
+                           plot(x12, y12, '+')
+                           plot(x13, y13, '+')
                            return
                        end
                        
@@ -148,79 +161,44 @@ classdef PerfectObservationSensor < handle
                        
                        if leftEdge(1) > RightIntersection || rightEdge(1) < LeftIntersection
                            output = [1, 1];
+                           line([x1, x2], [y1, y2]);
+                           fprintf("Inactive wall region")
                            return
                        end
                        
-                       [LB, IBL] =  max([LeftIntersection, leftEdge(1)]);
-                       [RB, IBR] =  min([RightIntersection, rightEdge(1)]);
+                       lefts = [leftBounds; leftEdge];
+                       rights = [rightBounds; rightEdge];
+                       
+                       [LB, IBL] =  max(lefts(:, 1));
+                       [RB, IBR] =  min(rights(:, 1));
                        
                        if LB < x_m && x_m < RB
-                           output = [x_m, y_m]
+                           output = [x_m, y_m];
                        else
-                           if norm(
-                               )
+                           if norm(lefts(IBL, :) - [x_m, y_m]) < norm(rights(IBR, :) - [x_m, y_m])
+                               output = lefts(IBL, :);
+                           else
+                               output = rights(IBR, :);
+                           end
+                       end
+                               
                        
 
-%                         if norm([x12, y12]) <= r && obj.VectorAngle(headingVector, [x12, y12]) < pi/2
-%                             
-%                             if norm()
-%                             
-%                             if norm(leftEdge) <= r && obj.VectorAngle(headingVector, leftEdge) < obj.alpha
-%                                 LeftLimit = leftEdge;                          
-%                             else
-%                                 LeftLimit = [x12, y12];
-%                             end
-%                         end
-%                         
-%                         if norm([x13, y13]) <= r && obj.VectorAngle(headingVector, [x13, y13]) < pi/2
-%                             if norm(rightEdge) <= r && obj.VectorAngle(headingVector, rightEdge) < obj.alpha
-%                                 RightLimit = rightEdge;
-%                             else
-%                                 RightLimit = [x12, y12];
-%                             end
-%                         end
-%                         
-%                         % New limits based on sensor cone geoemetry limits 
-%                         x_array = max([LeftLimit(1), RightLimit(1)], 0);
-%                         y_array = max([LeftLimit(2), RightLimit(2)], 0);
-%                         
-%                         edgeVectorLeft = [x_m - x_array(1), y_m - y_array(1)];
-%                         edgeVectorRight = [x_m - x_array(2), y_m - y_array(2)];
-%                         
-%                         leftDirVector = [x_limits(I2) - x_limits(I1),y_limits(I2) - y_limits(I1)];
-%                         rightDirVector = -leftDirVector;
-%                         
-%                         wallVectorLeft = [x_m - x_limits(I1),y_m - y_limits(I1)];
-%                         wallVectorRight = [x_m - x_limits(I1),y_m - y_limits(I1)];
-%                        
 
-%                         if x_array(1) > objMax || x_array(2) < objMin
-%                             % Wall lies outside of range
-%                             output = [1, 1];
-%                         else
-%                             
-%                             if objMin > x_array(1)
-%                                 x_array(1) = x_limits(I1);
-%                                 y_array(1) = y_limits(I1);
-%                             elseif objMax < max(x_limits)
-%                                 x_array(2) = x_limits(I2);
-%                                 y_array(2) = y_limits(I2);
-%                             end
-%                             
-%                             
-%                         end
-          
+                      plot(x12, y12, '+')
+                      plot(x13, y13, '+')
                     end
-                   output = [x_array(2), y_array(2)];
+                  %output = [x_array(2), y_array(2)];
+
 
                     
                 else
-                    output = [x1, y1];
+                    fprintf("No intersection occurred")
+                    output = [1, 1];
                 end
                     
                 line([x1, x2], [y1, y2]);
-                plot(x12, y12, '+')
-                plot(x13, y13, '+')
+                plot(output(1), output(2), 'o')
                 
            end
             
